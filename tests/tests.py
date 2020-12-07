83465a551f33ee5061dc71da6da07103daf08a94
Shavrin Maksim <nutmegraw@gmail.com>
2020-12-07 19:48:38 +0500

    Tests are separated

--- tests/tests.py
+++ tests/tests.py
@@ -12,27 +12,30 @@ class TestReporterMethods(TestCase):
         response = self.client.get('/api/v1/report/').get_data(as_text=True)
         response_json = self.client.get('/api/v1/report/?format=json').get_data(as_text=True)
         self.assertEqual(115, response_json.count('\n'))
-        response_xml = self.client.get('/api/v1/report/?format=xml').get_data(as_text=True)
-        self.assertIn('<report>', response_xml)
         self.assertEqual(response, response_json)
 
-    def test_show_report(self):
+    def test_get_report_formated_xml(self):
+        response_xml = self.client.get('/api/v1/report/?format=xml').get_data(as_text=True)
+        self.assertIn('<report>', response_xml)
+
+    def test_show_report_asc(self):
         response = self.client.get('/report/').get_data(as_text=True)
         response_asc = self.client.get('/report/?order=asc').get_data(as_text=True)
         sample = "<p>15 |Kevin Magnussen |HAAS FERRARI |0:01:13.393000</p>\n        \n          <p>------------------------------------------------------------------------</p>"
         self.assertIn(sample, response_asc)
+        self.assertEqual(response, response_asc)
 
+    def test_show_report_desc(self):
         response_desc = self.client.get('/report/?order=desc').get_data(as_text=True)
         sample_desc = "<p>16 |Daniel Ricciardo |RED BULL RACING TAG HEUER |0:02:47.987000</p>\n        \n          <p>------------------------------------------------------------------------</p>"
         self.assertIn(sample_desc, response_desc)
-
-        self.assertEqual(response, response_asc)
 
     def test_show_report_drivers(self):
         response_drivers = self.client.get('/report/drivers/').get_data(as_text=True)
         sample = 'Valtteri Bottas <a href="/report/drivers/?driver_id=VBM">VBM</a>'
         self.assertIn(sample, response_drivers)
 
+    def test_show_report_drivers_concreat_driver(self):
         response_spf = self.client.get('/report/drivers/?driver_id=SPF').get_data(as_text=True)
         self.assertIn('7 |FORCE INDIA MERCEDES |0:01:12.848000', response_spf)
         self.assertIn('Sergio Perez', response_spf)
